2cd2bc601d0a5853c7b70448022f60d607cc4eb5
hyobbeee <hyobbeee@localhost>
2022-04-26 23:28:06 +0900

    키보드로 입력받은 두 숫자의 합을 구한다.

--- src/hyobbeee/calendar/Calendar.java
+++ src/hyobbeee/calendar/Calendar.java
@@ -1,13 +1,31 @@
 package hyobbeee.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
+// 키보드로 입력받은 두 숫자의 합을 구한다.
+	
+//	public static void main(String[] args) {
+//		System.out.println("두 수를 입력하세요.");
+//		Scanner scan = new Scanner(System.in); //scanner클래스 객체 생성 /system.in : 키보드로 받는다.
+//		String[] data = scan.nextLine().split(",");
+//		int n = Integer.parseInt(data[0]);
+//		int m = Integer.parseInt(data[1]); 
+//		System.out.println("두수의 합은 " + n * m + " 입니다.");
+//	}
+
 	public static void main(String[] args) {
-		System.out.println("일  월  화 수  목  금 토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+		int a, b;
+		Scanner scanner = new Scanner(System.in);
+		String s1, s2;
+		System.out.println("두 수를 입력해 주세요.");
+		s1 = scanner.next(); // 
+		s2 = scanner.next();
+		a = Integer.parseInt(s1);
+		b = Integer.parseInt(s2);
+
+		System.out.printf("%d와 %d의 합은 %d입니다.", a, b, a + b); //printf => %d 자리 변수삽입 가
+		scanner.close();// import 파일 열었으면 닫기 
 	}
 }
