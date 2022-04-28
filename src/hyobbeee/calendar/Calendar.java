2bd73e56d57bb311409bd4bb37adcc749bd4926a
hyobbeee <hyobbeee@218.38.137.27>
2022-04-28 19:35:58 +0900

    반복문 종료/초기, 프롬프트 생성

--- src/hyobbeee/calendar/Calendar.java
+++ src/hyobbeee/calendar/Calendar.java
@@ -4,35 +4,37 @@ import java.util.Scanner;
 
 public class Calendar {
 
-//	반복횟수를 입력하세요.
-//	3 (엔터)
-//
-//	월을 입력하세요.
-//	1
-//	2
-//	3
-//
-//	1월은 31일까지 있습니다.
-//	2월은 28일까지 있습니다.
-//	3월은 31일까지 있습니다.
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
+	
+// 전단계와 비슷하게 반복입력을 받을 수 있게 구현한다.
+//	입력하는 곳 앞에 프롬프트를 출력한다.
+//	-1을 입력받는 경우 프로그램을 종료한다.
 
 	public static void main(String[] args) {
 
+		String PROMPT = "cal> ";
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
-		System.out.println("반복 횟수를 입력하세요.");
-		int times = scanner.nextInt();
+		int month = 1 ;
 		
-		for (int i = 0; i < times; i++) { // n번 반복하는 코드
+		while (true) { // month가 -1이 아닌동안 반복하겠다.
 			System.out.println("달을 입력하세요.");
-			int month = scanner.nextInt();
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if (month == -1) {
+				System.out.println("1~12 사이의 값만 입력할 수 있습니다.");
+				break;
+			}	
+			if (month >= 13) {
+				System.out.println("1~12 사이의 값만 입력할 수 있습니다.");
+				continue; // 만나면 반복의 처음으로 돌아간다. 
+			}
 			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month)); // 메소드 생성
 		}
 
@@ -40,6 +42,16 @@ public class Calendar {
 		scanner.close();
 	}
 }
+
+//		int month = scanner.nextInt();
+//
+//		for (int i = 0; i < month; i++) { // n번 반복하는 코드
+//			System.out.println("달을 입력하세요.");
+//			System.out.print(PROMPT);
+//			int month = scanner.nextInt();
+//			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month)); // 메소드 생성
+//		}
+
 /*			
 // 내가 만든 코드 
 		int i = 1;
