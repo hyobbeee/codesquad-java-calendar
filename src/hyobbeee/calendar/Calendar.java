2c8d7975fec6958237ee873a643f797799927110
hyobbeee <hyobbeee@192.168.35.74>
2022-04-29 00:22:48 +0900

    윤년 적용, PROMPT 이름 변경

--- src/hyobbeee/calendar/Calendar.java
+++ src/hyobbeee/calendar/Calendar.java
@@ -2,33 +2,47 @@ package hyobbeee.calendar;
 
 public class Calendar {
 
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 평년
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 윤년
 
-	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) // 연수가 4로 떨어지면 윤년 그리고 연수가 100으로 나눠지면 평년 혹은 연수가 400으로 나눠떨어지면
+																	// 윤년으로 둔다.
+			return true;
+		else
+			return false;
 	}
-	
+
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1]; // 배열은 첫번째 자리가 0부터 시작하기 때문에 1빼줌 
+		} else {
+			return MAX_DAYS[month - 1];
+		}
+	}
+
 //  월을 입력하면 해당월의 달력을 출력한다.
 //	달력은 모양은 1단계에서 작성한 모양으로 만든다.
 //	1일은 일요일로 정해도 무방하다.
 //	-1을 입력받기 전까지 반복 입력받는다.
 //	프롬프트를 출력한다.
-	
+
 	public void printCalendar(int year, int month) {
-		System.out.printf("     <<%4d년%2d월>>\n",year, month); // % 뒤의숫자는 차지하는 자릿
+		System.out.printf("     <<%4d년%2d월>>\n", year, month); // % 뒤의숫자는 차지하는 자릿
 		System.out.println(" SU MO TU WE TH FI SA");
 		System.out.println("----------------------");
+
 		
-		int maxDay = getMaxDaysOfMonth(month);
 		
-		for(int i = 1; i <= maxDay; i++) {
-			System.out.printf("%3d", i); // %3d : 스페이스 포함 3칸 자리를 차지한다. 
-			if (i % 7 == 0) // 7 나눠서 나머지가 0 일때마다 줄바꿈 > 7번 돌때마다 
+		int maxDay = getMaxDaysOfMonth(year, month);
+
+		for (int i = 1; i <= maxDay; i++) {
+			System.out.printf("%3d", i); // %3d : 스페이스 포함 3칸 자리를 차지한다.
+			if (i % 7 == 0) // 7 나눠서 나머지가 0 일때마다 줄바꿈 > 7번 돌때마다
 				System.out.println();
 		}
 		System.out.println();
-		
+
 //		System.out.println(" 1  2  3  4  5  6  7");
 //		System.out.println(" 8  9 10 11 12 13 14");
 //		System.out.println("15 16 17 18 19 20 21");
--- src/hyobbeee/calendar/Prompt.java
+++ src/hyobbeee/calendar/Prompt.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
 
 public class Prompt {
 	
-	private final static String PROMPT = "cal> "; //변경이 없을시 대문자로 표기
+	private final static String PROMPT = "calendar > "; //변경이 없을시 대문자로 표기
 	
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		int month = 1;
+		int year = -1; // 디폴트 값 아무거나 해도 상관X
 
 		while (true) { // 무한루프
+			System.out.println("연도를 입력하세요.");
+			System.out.print("YEAR> ");
+			year = scanner.nextInt();
 			System.out.println("달을 입력하세요.");
-			System.out.print(PROMPT);
+			System.out.print("MONTH> ");
 			month = scanner.nextInt(); // int형 정수를 입력받는 매서드
 			if (month == -1) {
 				System.out.println("1~12 사이의 값만 입력할 수 있습니다.");
@@ -24,7 +28,7 @@ public class Prompt {
 				System.out.println("1~12 사이의 값만 입력할 수 있습니다.");
 				continue; // 만나면 반복의 처음으로 돌아간다.
 			}
-			cal.printCalendar(2022, month); //
+			cal.printCalendar(year, month); //
 		}
 
 		System.out.println("--반복 횟수 종료--");
