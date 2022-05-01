5c8102d3867834ad8ecc44ceade79b70b3f958f6
hyobbeee <hyobbeee@218.38.137.27>
2022-05-02 00:00:55 +0900

    Add print calendar function

--- src/hyobbeee/calendar/Calendar.java
+++ src/hyobbeee/calendar/Calendar.java
@@ -2,12 +2,11 @@ package hyobbeee.calendar;
 
 public class Calendar {
 
-	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 평년
-	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 윤년
+	private static final int[] LAST_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 평년
+	private static final int[] LEAP_LAST_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 윤년
 
 	public boolean isLeapYear(int year) {
-		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) // 연수가 4로 떨어지면 윤년 그리고 연수가 100으로 나눠지면 평년 혹은 연수가 400으로 나눠떨어지면
-																	// 윤년으로 둔다.
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) // 연수가 4로 떨어지면 윤년 그리고 연수가 100으로 나눠지면 평년 혹은 연수가 400으로 나눠떨어지면 윤년으로 둔다.
 			return true;
 		else
 			return false;
@@ -15,37 +14,51 @@ public class Calendar {
 
 	public int getMaxDaysOfMonth(int year, int month) {
 		if (isLeapYear(year)) {
-			return LEAP_MAX_DAYS[month - 1]; // 배열은 첫번째 자리가 0부터 시작하기 때문에 1빼줌 
+			return LEAP_LAST_DAYS[month - 1]; // 배열은 첫번째 자리가 0부터 시작하기 때문에 1빼줌 
 		} else {
-			return MAX_DAYS[month - 1];
+			return LAST_DAYS[month - 1];
 		}
 	}
 
-//  월을 입력하면 해당월의 달력을 출력한다.
-//	달력은 모양은 1단계에서 작성한 모양으로 만든다.
-//	1일은 일요일로 정해도 무방하다.
-//	-1을 입력받기 전까지 반복 입력받는다.
-//	프롬프트를 출력한다.
+//	월을 입력받는다.
+//	1일의 요일을 입력받는다.
+//	출력한다.
 
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("     <<%4d년%2d월>>\n", year, month); // % 뒤의숫자는 차지하는 자릿
-		System.out.println(" SU MO TU WE TH FI SA");
+		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("----------------------");
-
 		
+		//print blank space
+				for (int i = 0; i < weekday; i++) {
+					System.out.print("   ");
+				}
 		
 		int maxDay = getMaxDaysOfMonth(year, month);
-
-		for (int i = 1; i <= maxDay; i++) {
+		int count = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
+		/*
+		 if (count < 7){
+		 	delim = count;
+		 } else {
+		 	delim = 0;
+		 }	
+		 */
+		
+		//print first line
+		for(int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println(); // 줄바꿈 문자
+		
+		//print from second line to last
+		count++;	// 4부터 숫자 출력?
+		for (int i = count; i <= maxDay; i++) {
 			System.out.printf("%3d", i); // %3d : 스페이스 포함 3칸 자리를 차지한다.
-			if (i % 7 == 0) // 7 나눠서 나머지가 0 일때마다 줄바꿈 > 7번 돌때마다
-				System.out.println();
+			if (i % 7 == delim) // 7 나눠서 나머지가 delim 일때마다 줄바꿈 > 7번 돌때마다
+				System.out.println(); // 줄바꿈 문자
 		}
 		System.out.println();
-
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
+		System.out.println();
 	}
 }
--- src/hyobbeee/calendar/Prompt.java
+++ src/hyobbeee/calendar/Prompt.java
@@ -3,32 +3,55 @@ package hyobbeee.calendar;
 import java.util.Scanner;
 
 public class Prompt {
+
+	/**
+	 * 
+	 * @param week : 요일 
+	 * @return 0 ~ 6 (0 = Sunday, 6 = Saturday)
+	 */
 	
-	private final static String PROMPT = "calendar > "; //변경이 없을시 대문자로 표기
+	public int parseDay(String week) {
+		if (week.equals("SU")) return 0; 
+		else if (week.equals("MO")) return 1;
+		else if (week.equals("TU")) return 2;
+	 	else if (week.equals("WE")) return 3;
+	 	else if (week.equals("TH")) return 4;
+	 	else if (week.equals("FR")) return 5;
+	 	else if (week.equals("SA")) return 6;
+	 	else
+			return 0; // 혹시 이상한거 하면 0번으로 한다.
+		} 
 	
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
+		int year = 2022; // 디폴트 값 아무거나 해도 상관X
 		int month = 1;
-		int year = -1; // 디폴트 값 아무거나 해도 상관X
+		int weekday = 0;
 
 		while (true) { // 무한루프
-			System.out.println("연도를 입력하세요.");
+			System.out.println("연도를 입력하세요.(exit : -1)");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
+			if (year == -1)
+				break; // 무한루프 빠져나가는 코드
+			
 			System.out.println("달을 입력하세요.");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt(); // int형 정수를 입력받는 매서드
-			if (month == -1) {
-				System.out.println("1~12 사이의 값만 입력할 수 있습니다.");
-				break;
-			}
-			if (month >= 13) {
-				System.out.println("1~12 사이의 값만 입력할 수 있습니다.");
+			
+			System.out.println("첫번째 요일을 입력하세요.(SU, MO, TU, WE, TH, FR, SA)");
+			System.out.print("WEEKDAY> ");
+//			char weekday = scanner.next().charAt(0); // 입력문자의 첫번째 값을 받는다.
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+			
+			if (month > 12 || month < 1) {
+				System.err.println("1~12월 사이의 값만 입력할 수 있습니다.");
 				continue; // 만나면 반복의 처음으로 돌아간다.
 			}
-			cal.printCalendar(year, month); //
+			cal.printCalendar(year, month, weekday);
 		}
 
 		System.out.println("--반복 횟수 종료--");
